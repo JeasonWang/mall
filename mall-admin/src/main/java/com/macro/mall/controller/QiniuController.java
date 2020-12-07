af5211638f3d5e3d2e157e93f5957182e767c752
wanghuan <2578145258@qq.com>
2020-12-08 01:25:17 +0800

    临时提交

---
+++ mall-admin/src/main/java/com/macro/mall/controller/QiniuController.java
@@ -0,0 +1,42 @@
+package com.macro.mall.controller;
+
+
+import com.macro.mall.common.api.CommonResult;
+import com.macro.mall.common.util.IdUtils;
+import com.macro.mall.common.util.QiniuUtil;
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Qiniu相关操作接口
+ * Created by macro on 2018/4/26.
+ */
+@Controller
+@Api(tags = "QiniuController", description = "Qiniu管理")
+@RequestMapping("/qiniuyun/qiniu")
+public class QiniuController {
+    @Resource
+    private QiniuUtil qiniuUtil;
+
+    /**
+     * 上传文件到七牛云存储
+     * @param multipartFile
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/img/upload")
+    public CommonResult uploadImgQiniu(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+        FileInputStream inputStream = (FileInputStream) multipartFile.getInputStream();
+        String url = qiniuUtil.uploadImg(inputStream, String.valueOf(IdUtils.snowflakeId()));
+        return CommonResult.success("上传成功",url);
+    }
+
+}
