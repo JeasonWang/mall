df89a8d08ff1e38ea9e0f3681f860dab580e0c0e
wanghuan <2578145258@qq.com>
2020-12-21 14:25:25 +0800

    添加mq模块

---
+++ mall-mq/src/test/java/com/macro/mall/mallmq/MallMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macro.mall.mallmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
