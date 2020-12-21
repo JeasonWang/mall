92054050f6a0eaf0881d04635738b27b17901c45
wanghuan <2578145258@qq.com>
2020-12-21 16:59:15 +0800

    添加mq模块

---
+++ mall-resthighlevel/src/main/java/com/macro/mall/mallresthighlevel/MallResthighlevelApplication.java
@@ -0,0 +1,13 @@
+package com.macro.mall.mallresthighlevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallResthighlevelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallResthighlevelApplication.class, args);
+    }
+
+}
---
+++ mall-resthighlevel/src/test/java/com/macro/mall/mallresthighlevel/MallResthighlevelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macro.mall.mallresthighlevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallResthighlevelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
