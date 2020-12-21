76d26f11905dea3a8a1cb33306dacd48bb4b88f6
wanghuan <2578145258@qq.com>
2020-12-22 00:18:47 +0800

    mq实现削峰限流

---
+++ mall-mq/src/main/java/com/macro/mall/mallmq/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.macro.mall.mallmq.config;
+
+import com.macro.mall.common.config.BaseRedisConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: jeason
+ * @Description:
+ * @Date: 2020/12/20 21:49
+ */
+@Configuration
+public class RedisConfig extends BaseRedisConfig {
+}
