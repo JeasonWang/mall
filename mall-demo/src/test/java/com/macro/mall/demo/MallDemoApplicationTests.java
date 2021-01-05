df5d5bddc1de24c364f928cc7deb76fc6fc43753
wanghuan <2578145258@qq.com>
2021-01-06 00:07:28 +0800

    rabbitmq消息失败重试3次

--- mall-demo/src/test/java/com/macro/mall/demo/MallDemoApplicationTests.java
+++ mall-demo/src/test/java/com/macro/mall/demo/MallDemoApplicationTests.java
@@ -1,11 +1,13 @@
-package com.macro.mall;
+package com.macro.mall.demo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.macro.mall.mapper.PmsBrandMapper;
 import com.macro.mall.model.PmsProduct;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,10 +17,14 @@ public class MallDemoApplicationTests {
 	private Logger logger = LoggerFactory.getLogger(MallDemoApplicationTests.class);
 	@Test
 	public void contextLoads() {
+		System.out.println("hello");
 	}
 
+	@Autowired
+	PmsBrandMapper pmsBrandMapper;
 	@Test
 	public void testLogStash() throws Exception {
+		pmsBrandMapper.selectByPrimaryKey(1L);
 		ObjectMapper mapper = new ObjectMapper();
 		PmsProduct product = new PmsProduct();
 		product.setId(1L);
